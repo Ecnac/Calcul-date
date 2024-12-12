1b65770b2191ab56fe5c928ede93ce8956790a75
Ecnac <101355118+Ecnac@users.noreply.github.com>
2024-12-12 10:23:36 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ const calculateDifference = () => {
     const service = document.getElementById('services').value;
     let fees = days;
 
-    if (service == "") {
+    if (service === "") {
         alert("Veuillez sélectionner un service.");
         return;
     }
